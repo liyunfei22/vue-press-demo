6ea3635a53bd3cd368dc8ece242aa48e54a0c9a0
李云飞 <liyunfei02@daojia-inc.com>
2021-09-08 19:04:13 +0800

    feat: init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   lang: 'zh-CN',
   title: 'hello ！',
   description: '这是我的第一个 VuePress 站点',
-
+  base: '/vue-press-demo/',
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
   },
